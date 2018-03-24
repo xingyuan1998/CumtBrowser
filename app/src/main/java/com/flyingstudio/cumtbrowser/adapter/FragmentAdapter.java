cfce406d2a9fabde3da55afe172ee49d896c6292
yuanxing <yuanxing@sohu-inc.com>
2018-03-24 22:14:39 +0800

    Initial project commit

---
+++ app/src/main/java/com/flyingstudio/cumtbrowser/adapter/FragmentAdapter.java
@@ -0,0 +1,31 @@
+package com.flyingstudio.cumtbrowser.adapter;
+
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentPagerAdapter;
+
+import java.util.List;
+
+/**
+ * Created by yuan on 18-3-24.
+ */
+
+public class FragmentAdapter extends FragmentPagerAdapter {
+    private List<Fragment> fragmentList;
+
+
+    public FragmentAdapter(FragmentManager fm, List<Fragment> fragments) {
+        super(fm);
+        this.fragmentList = fragments;
+    }
+
+    @Override
+    public Fragment getItem(int position) {
+        return fragmentList.get(position);
+    }
+
+    @Override
+    public int getCount() {
+        return fragmentList.size();
+    }
+}
